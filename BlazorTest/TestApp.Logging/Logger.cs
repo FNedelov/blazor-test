b934cfa784091eae462147651fff308c43c50ec3
nedferi <nedferi@gmail.com>
2023-01-08 15:33:41 +0100

    Created custom error boundary.

--- BlazorTest/TestApp.Logging/Logger.cs
+++ BlazorTest/TestApp.Logging/Logger.cs
@@ -2,19 +2,19 @@
 {
     public static class Logger
     {
-        public static void AppendLogLineSync(string logDirectory, string fileName, string text)
+        public static void AppendLogLineSync(string filePath, string fileName, string text)
         {
             string line = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} \t - {text}";
-            string logfile = @$"{logDirectory}{DateTime.Now.Year:yyyy-MM-dd HH:mm:ss}_{fileName}.log";
+            string logfile = @$"{filePath}{DateTime.Now.Year:yyyy-MM-dd HH:mm:ss}_{fileName}.log";
 
             using StreamWriter w = File.AppendText(logfile);
             w.WriteLine(line);
         }
 
-        public static async Task AppendLogLineAsync(string logDirectory, string fileName, string text)
+        public static async Task AppendLogLineAsync(string filePath, string fileName, string text)
         {
             string line = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} \t - {text}";
-            string logfile = @$"{logDirectory}{DateTime.Now.Year:yyyy-MM-dd HH:mm:ss}_{fileName}.log";
+            string logfile = @$"{filePath}{DateTime.Now.Year:yyyy-MM-dd HH:mm:ss}_{fileName}.log";
 
             using StreamWriter w = File.AppendText(logfile);
             await w.WriteLineAsync(line);
---
+++ BlazorTest/TestApp.Web/HelperClasses/CustomErrorBoundary.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components;
+using TestApp.Logging;
+
+namespace TestApp.Web.HelperClasses
+{
+    public class CustomErrorBoundary : ErrorBoundary
+    {
+        [Inject]
+        private IWebHostEnvironment _env { get; set; } = null!;
+        protected override async Task OnErrorAsync(Exception ex)
+        {
+            await Logger.AppendLogLineAsync(AppData.LOG_FILE_PATH, AppData.LOG_FILE_NAME, $"Exception message:\t{ex.Message}");
+            await Logger.AppendLogLineAsync(AppData.LOG_FILE_PATH, AppData.LOG_FILE_NAME, $"Inner exception:\t{ex.InnerException}");
+            await Logger.AppendLogLineAsync(AppData.LOG_FILE_PATH, AppData.LOG_FILE_NAME, $"Exception stack trace:\t{ex.StackTrace}");
+
+            if (_env.IsDevelopment())
+            {
+                await base.OnErrorAsync(ex);
+                return;
+            }
+            return;
+        }
+    }
+}
